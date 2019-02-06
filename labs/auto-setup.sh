07e02763e99b32df0e7b52643a420d455a721742
Rob Martin <robmartin@arista.com>
2019-02-05 23:34:23 -0600

    Cleaned up

--- labs/auto-setup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "============ Creating Base Lab Environment ================="
-echo "[Creating necessary directories]"
-mkdir class/group_vars
-echo "[Copying base Ansible Files]"
-cp completed/ansible.cfg class/
-cp completed/hosts class/
-echo "============ Done! ================="
